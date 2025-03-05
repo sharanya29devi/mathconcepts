551e2c9bb7b73618a4d24be71fada11d111887b2
Sharanya Devi <141646902+sharanya29devi@users.noreply.github.com>
2025-03-05 16:43:35 +0530

    Update problems.py

--- problems.py
+++ problems.py
@@ -14,11 +14,28 @@ while (n>0):
 print(c)
 
 #oddDigit
-n = 23456
+n = 7654322
 c = 0
 while (n>0):
     ld = n%10
-    if (ld%2==0):
-        c = c+1
-        n = n//10
+    if (ld%2!=0):
+        c=c+1
+    n = n // 10
 print(c)
+
+#power&kthDigitFromLast
+a = 8
+b = 3
+c = 1
+d = 1
+for i in range(1,b+1):
+    d = d*a
+s = 0
+print(d)
+while (d>0):
+    l=d%10
+    s=s+1
+    if (s==c):
+        print(l)
+        break
+    d=d//10
