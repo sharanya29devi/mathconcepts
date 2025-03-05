1424a6295a341083392cfa0ec963282e74a8d6dc
Sharanya Devi <141646902+sharanya29devi@users.noreply.github.com>
2025-03-05 12:28:03 +0530

    Update problems.py

--- problems.py
+++ problems.py
@@ -1,8 +1,24 @@
-#lastdigit&divisibility
-
+#lastDigit&Divisibility
 n = 3456
 while (n>0):
     ld = n%10
     print(ld)
     n=n//10
 
+#countOfDigitInNumber
+n = 234567
+c = 0
+while (n>0):
+    c = c + 1
+    n //= 10
+print(c)
+
+#oddDigit
+n = 23456
+c = 0
+while (n>0):
+    ld = n%10
+    if (ld%2==0):
+        c = c+1
+        n = n//10
+print(c)
